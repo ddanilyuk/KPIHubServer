56318f3378aaa7ffe13624c7610ee511f4dc75bf
ddanilyuk <danis.danilyuk@gmail.com>
2022-06-04 00:46:19 +0300

    Add CampusLoginQuery public init

--- Sources/Routes/Models/CampusLoginQuery.swift
+++ Sources/Routes/Models/CampusLoginQuery.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 public struct CampusLoginQuery {
+
     public let username: String
     public let password: String
+
+    public init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
 }
 
 // MARK: Equatable
